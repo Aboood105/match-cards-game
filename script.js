334f6a00c0ad5d4a58ad414b95661622efd7471a
Aboood105 <orcs882@gmail.com>
2026-02-08 16:34:46 +0300

    update

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const startButton = document.querySelector(`#startButton`)
 const resetButton = document.querySelector(`#resetButton`)
+const time = document.querySelector(`#timer`)
+
+let timeLeft = 60
+
+function reset() {
+  timeLeft = 60
+}
+resetButton.addEventListener(`click`, reset)
+
+const timer = setInterval(() => {
+  time.innerHTML = `<h3> time :${timeLeft}</h3>`
+  timeLeft--
+
+  if (timeLeft < 0) {
+    clearInterval(timer)
+    time.innerHTML = `<h3> Time's up! </h3> `
+  }
+}, 100)

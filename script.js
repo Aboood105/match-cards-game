51c02ac6ae944b0a7506d91a0064529d61861a73
Aboood105 <orcs882@gmail.com>
2026-02-09 16:04:22 +0300

    update imgs and folder

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+//////////////////////////////////////
+// const
 const startButton = document.querySelector(`#startButton`)
 const resetButton = document.querySelector(`#resetButton`)
 const time = document.querySelector(`#timer`)
 const mainBoard = document.querySelector(`#mainBoard`)
 const img = document.querySelectorAll(`.photo`)
 // console.log(img.src)
-let arraySrc = []
-// console.log(arraySrc)
-let timeLeft = 60
 
+// console.log(arraySrc)
+
+////////////////////////////////////////
+///// let
+
+let arraySrc = []
+let timeLeft = 60
+let imgMaths = [
+  `img-html/hhh.png`,
+  `img-html/aa.png`,
+  `img-html/33.png`,
+  `img-html/44.jpg`,
+]
+
+let random = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+
+img.forEach((singleImg) => {
+  const creatImg1 = document.createElement("img")
+  creatImg1.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+  creatImg1.classList.add("photo")
+  singleImg.appendChild(creatImg1)
+})
+
+////////////////////////////////////
+// function
 function reset() {
   timeLeft = 60
 }
@@ -16,7 +40,6 @@ resetButton.addEventListener(`click`, reset)
 const timer = setInterval(() => {
   time.innerHTML = `<h3> time :${timeLeft}</h3>`
   timeLeft--
-
   if (timeLeft < 0) {
     clearInterval(timer)
     time.innerHTML = `<h3> Time's up! </h3> `
@@ -24,14 +47,11 @@ const timer = setInterval(() => {
 }, 1000)
 
 function doubleImg() {
-  for (i = 0; i < img.length; i++) {
-    const src = img[i].src
+  for (i = 0; i < imgMaths.length; i++) {
     const creatImg = document.createElement("img")
-    creatImg.src = src
-    creatImg.className = "photo"
+    creatImg.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+    creatImg.classList.add(`photo`)
     mainBoard.appendChild(creatImg)
   }
 }
-// console.log(img[11].src)
-
 doubleImg()

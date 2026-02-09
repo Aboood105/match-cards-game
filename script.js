013e4a781ca13ee399d20277038448c7ed12e227
Aboood105 <orcs882@gmail.com>
2026-02-09 10:42:55 +0300

    double img function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const startButton = document.querySelector(`#startButton`)
 const resetButton = document.querySelector(`#resetButton`)
 const time = document.querySelector(`#timer`)
-
+const mainBoard = document.querySelector(`#mainBoard`)
+const img = document.querySelectorAll(`.photo`)
+// console.log(img.src)
+let arraySrc = []
+// console.log(arraySrc)
 let timeLeft = 60
 
 function reset() {
@@ -17,4 +21,17 @@ const timer = setInterval(() => {
     clearInterval(timer)
     time.innerHTML = `<h3> Time's up! </h3> `
   }
-}, 100)
+}, 1000)
+
+function doubleImg() {
+  for (i = 0; i < img.length; i++) {
+    const src = img[i].src
+    const creatImg = document.createElement("img")
+    creatImg.src = src
+    creatImg.className = "photo"
+    mainBoard.appendChild(creatImg)
+  }
+}
+// console.log(img[11].src)
+
+doubleImg()

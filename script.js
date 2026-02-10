afc33b4cece835722b8dc8bce5fec284f5887d8d
Aboood105 <orcs882@gmail.com>
2026-02-11 01:25:40 +0300

    go to sleep

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const resetButton = document.querySelector(`#resetButton`)
 const time = document.querySelector(`#timer`)
 const mainBoard = document.querySelector(`#mainBoard`)
 const img = document.querySelectorAll(`.photo`)
-// console.log(img.src)
-
-// console.log(arraySrc)
 
 ////////////////////////////////////////
 ///// let
@@ -20,45 +17,55 @@ let imgMaths = [
   `img-html/aa.png`,
   `img-html/33.png`,
   `img-html/44.jpg`,
-  `/img-html/55.png`,
-  `/img-html/66.png`,
-  `/img-html/77.png`,
-  `/img-html/88.jpg`,
-]
+  `img-html/55.png`,
+  `img-html/66.png`,
+  `img-html/77.png`,
+  `img-html/88.jpg`,
+] //more img
 
-let random = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+// function startGame() {
+//   mainBoard.innerHTML = ``
+//   flippedImg = []
+//   count = 0
+//   timeLeft = 60
 
-function startGame() {
-  mainBoard.innerHTML = ``
-  flippedImg = []
-  count = 0
-  timeLeft = 60
+//   let allImg = [...imgMaths, ...imgMaths]
 
-  let allImg = [...imgMaths, ...imgMaths]
+//   allImg.random
+//   allImg.sort(() => Math.random() - 0.5)
 
-  allImg.random
-  allImg.sort(() => Math.random() - 0.5)
+//   allImg.forEach((divs) => {
+//     const card = document.createElement("div")
+//     card.classList.add("photo")
+//     card.classList.add("flipped")
 
-  allImg.forEach((divs) => {
-    const card = document.createElement("div")
-    card.classList.add("photo")
+//     const front = document.createElement("img")
+//     // front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+//     front.src = divs
+//     front.classList.add("front")
 
-    const front = document.createElement("img")
-    // front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
-    front.src = divs
-    front.classList.add("front")
+//     const back = document.createElement("div")
+//     back.classList.add("back")
 
-    const back = document.createElement("div")
-    back.classList.add("back")
+//     card.appendChild(back)
+//     card.appendChild(front)
 
-    card.appendChild(back)
-    card.appendChild(front)
+//     card.addEventListener("click", flipImg)
+//     mainBoard.appendChild(card)
+//   })
+//   // 2. WAIT 5 SECONDS: Then flip them back and enable clicking
+//   time.innerHTML = `<h3>Memorize the cards!</h3>`
 
-    card.addEventListener("click", flipImg)
-    mainBoard.appendChild(card)
-  })
-  startTimer()
-}
+//   setTimeout(() => {
+//     const allCards = document.querySelectorAll(".photo")
+//     allCards.forEach((card) => {
+//       card.classList.remove("flipped") // Flip them back to hide the image
+//       card.addEventListener("click", flipImg) // ONLY enable clicking after the peek
+//     })
+//     // 3. START GAME: Start the countdown timer only after the peek is over
+//     startTimer()
+//   }, 3000)
+// }
 // img.forEach((divs) => {
 //   const front = document.createElement("img")
 //   front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
@@ -86,11 +93,68 @@ function startGame() {
 
 ////////////////////////////////////
 // function
+
+function startGame() {
+  mainBoard.innerHTML = ``
+  flippedImg = []
+  count = 0
+  timeLeft = 60
+
+  let allImg = [...imgMaths, ...imgMaths]
+  allImg.random
+  // maybe this
+  // allImg.sort(() => Math.random() - 0.5)
+  // or this
+  // for (let i = allImg.length - 1; i > 0; i--) {
+  //   const j = Math.floor(Math.random() * (i + 1))
+  //   let temp = allImg[i]
+  //   allImg[i] = allImg[j]
+  //   allImg[j] = temp
+  // }
+  // or this
+  for (let i = 0; i < allImg.length; i++) {
+    const j = Math.floor(Math.random() * allImg.length)
+    let temp = allImg[i]
+    allImg[i] = allImg[j]
+    allImg[j] = temp
+  }
+
+  allImg.forEach((divs) => {
+    const card = document.createElement("div")
+    card.classList.add("photo")
+    card.classList.add("flipped")
+
+    const front = document.createElement("img")
+    // front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+    front.src = divs
+    front.classList.add("front")
+
+    const back = document.createElement("div")
+    back.classList.add("back")
+
+    card.appendChild(back)
+    card.appendChild(front)
+
+    // card.addEventListener("click", flipImg)
+    mainBoard.appendChild(card)
+  })
+
+  time.innerHTML = `<h3>Memorize the cards!</h3>`
+  setTimeout(() => {
+    const allCards = document.querySelectorAll(".photo")
+
+    allCards.forEach((card) => {
+      card.classList.remove("flipped")
+      card.addEventListener("click", flipImg)
+    })
+    startTimer()
+  }, 3000)
+}
 // let flippedImg = []
 function flipImg() {
   if (flippedImg.length === 2 || this.classList.contains(`flipped`)) return
   this.classList.add(`flipped`)
-  flippedImg.push(`this`)
+  flippedImg.push(this)
   if (flippedImg.length === 2) {
     checkCard()
   }
@@ -100,9 +164,11 @@ function checkCard() {
   const [card1, card2] = flippedImg
   const isMatched =
     card1.querySelector(`.front`).src === card2.querySelector(`.front`).src
+
   if (isMatched) {
     count++
     flippedImg = []
+
     if (count === imgMaths.length) {
       clearInterval(timer)
       time.innerHTML = `<h3> you win </h3> `

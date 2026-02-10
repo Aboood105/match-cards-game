4bd5de362c6754f399f4d4dd5be784d58bd9ca63
Aboood105 <orcs882@gmail.com>
2026-02-10 14:53:02 +0300

    some error in js

--- script.js
+++ script.js
@@ -11,59 +11,137 @@ const img = document.querySelectorAll(`.photo`)
 
 ////////////////////////////////////////
 ///// let
+let flippedImg = []
+let count = 0
 let timer
 let timeLeft = 60
 let imgMaths = [
   `img-html/hhh.png`,
   `img-html/aa.png`,
-  `/index.html/`,
-  // `img-html/33.png`,
-  // `img-html/44.jpg`,
-  // `/img-html/55.png`,
-  // `/img-html/66.png`,
-  // `/img-html/77.png`,
-  // `/img-html/88.jpg`,
+  `img-html/33.png`,
+  `img-html/44.jpg`,
+  `/img-html/55.png`,
+  `/img-html/66.png`,
+  `/img-html/77.png`,
+  `/img-html/88.jpg`,
 ]
-
-img.forEach((divs) => {
-  const front = document.createElement("img")
-  front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
-  front.classList.add("front")
-
-  const back = document.createElement("img")
-  back.classList.add("back")
-
-  divs.appendChild(back)
-  divs.appendChild(front)
-
-  divs.addEventListener("click", () => {
-    divs.classList.toggle("flipped")
-  })
-})
 
 let random = imgMaths[Math.floor(Math.random() * imgMaths.length)]
 
-img.forEach((singleImg) => {
-  const creatImg1 = document.createElement("img")
-  creatImg1.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
-  creatImg1.classList.add("photo")
-  singleImg.appendChild(creatImg1)
-})
+function startGame() {
+  mainBoard.innerHTML = ``
+  flippedImg = []
+  count = 0
+  timeLeft = 60
+
+  let allImg = [...imgMaths, ...imgMaths]
+
+  allImg.random
+  allImg.sort(() => Math.random() - 0.5)
+
+  allImg.forEach((divs) => {
+    const card = document.createElement("div")
+    card.classList.add("photo")
+
+    const front = document.createElement("img")
+    // front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+    front.src = divs
+    front.classList.add("front")
+
+    const back = document.createElement("div")
+    back.classList.add("back")
+
+    card.appendChild(back)
+    card.appendChild(front)
+
+    card.addEventListener("click", flipImg)
+    mainBoard.appendChild(card)
+  })
+  startTimer()
+}
+// img.forEach((divs) => {
+//   const front = document.createElement("img")
+//   front.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+//   front.classList.add("front")
+
+//   const back = document.createElement("img")
+//   back.classList.add("back")
+
+//   divs.appendChild(back)
+//   divs.appendChild(front)
+
+//   divs.addEventListener("click", () => {
+//     divs.classList.toggle("flipped")
+//   })
+// })
+
+// let random = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+
+// img.forEach((singleImg) => {
+//   const creatImg1 = document.createElement("img")
+//   creatImg1.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+//   creatImg1.classList.add("photo")
+//   singleImg.appendChild(creatImg1)
+// })
 
 ////////////////////////////////////
 // function
-
-function doubleImg() {
-  for (i = 0; i < imgMaths.length; i++) {
-    const creatImg = document.createElement("img")
-    creatImg.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
-    creatImg.classList.add(`photo`)
-    mainBoard.appendChild(creatImg)
+// let flippedImg = []
+function flipImg() {
+  if (flippedImg.length === 2 || this.classList.contains(`flipped`)) return
+  this.classList.add(`flipped`)
+  flippedImg.push(`this`)
+  if (flippedImg.length === 2) {
+    checkCard()
   }
 }
-doubleImg()
 
-function reset() {
-  timeLeft = 60
+function checkCard() {
+  const [card1, card2] = flippedImg
+  const isMatched =
+    card1.querySelector(`.front`).src === card2.querySelector(`.front`).src
+  if (isMatched) {
+    count++
+    flippedImg = []
+    if (count === imgMaths.length) {
+      clearInterval(timer)
+      time.innerHTML = `<h3> you win </h3> `
+    }
+  } else {
+    setTimeout(() => {
+      card1.classList.remove("flipped")
+      card2.classList.remove("flipped")
+      flippedImg = []
+    }, 1000)
+  }
 }
-resetButton.addEventListener(`click`, reset)
+
+function startTimer() {
+  clearInterval(timer)
+  timer = setInterval(() => {
+    timeLeft--
+    time.innerHTML = `<h3> time :${timeLeft}</h3>`
+
+    if (timeLeft <= 0) {
+      clearInterval(timer)
+      time.innerHTML = `<h3> Time's up! </h3> `
+    }
+  }, 1000)
+}
+// startTimer()
+// no need for it ^^^^^
+// function doubleImg() {
+//   for (i = 0; i < imgMaths.length; i++) {
+//     const creatImg = document.createElement("img")
+//     creatImg.src = imgMaths[Math.floor(Math.random() * imgMaths.length)]
+//     creatImg.classList.add(`photo`)
+//     mainBoard.appendChild(creatImg)
+//   }
+// }
+// doubleImg()
+
+// function reset() {
+//   timeLeft = 60
+// }
+resetButton.addEventListener(`click`, startGame)
+startGame()
